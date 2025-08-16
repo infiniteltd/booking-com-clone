4ac1eb03e5d5adf342bdaf8209515b857ce7e7ef
infiniteltd <chrissamps24@gmail.com>
2025-08-16 22:20:21 +0100

    new commit

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -118,12 +118,55 @@ function Header() {
                           aria-hidden="true"
                         />
                       </div>
+
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-[#013894]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-[#013894]">
+                          {item.description}
+                        </p>
+                      </div>
                     </div>
+                  ))}
+                </div>
+
+                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                  {callsToAction.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6
+                  text-[#013894] hover:bg-gray-100"
+                    >
+                      <item.icon
+                        className="h-5 w-5 flex-none text-[#013894]"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </a>
                   ))}
                 </div>
               </PopoverPanel>
             </Transition>
           </Popover>
+
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Flights
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Car Rentals
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Site Attractions
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Flight + Hotel
+          </a>
         </PopoverGroup>
       </nav>
     </header>
